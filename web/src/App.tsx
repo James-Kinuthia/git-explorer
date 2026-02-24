21644117d9bcbdf960725b1775ae2077b64ac71c
James-Kinuthia <kinuthiaa22019@gmail.com>
2026-02-24 19:55:49 +0300

    sorting and management
    Made some folder changes

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="bg-slate-900 flex flex-col space-y-4 min-h-full h-screen w-screen items-center justify-center">
+      <h1 className="text-6xl font-extrabold mb-3 text-green-500 text-center">Hey Jimmy</h1>
+      <p className="text-base leading-1 mt-4 text-slate-100">This was reset to blank so we can get into some serious grinding with 
+         <span className="text-blue-500">TypeScript</span> and <strong className="text-blue-800">TailwindCSS</strong>
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
